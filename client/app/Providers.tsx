5a1e6a943a3baf63d6c645ee9ba990dce8412fd2
Deeapk Singh <deepakksingh1202@gmail.com>
2026-05-18 15:12:49 +0530

    Update application context providers configuration

--- client/app/Providers.tsx
+++ client/app/Providers.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import queryClient from "../lib/queryClient";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "../store/reduxStore";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <ReduxProvider store={store}>
       {children}
-      {process.env.NODE_ENV === "development" && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
-    </QueryClientProvider>
+    </ReduxProvider>
   );
 }
