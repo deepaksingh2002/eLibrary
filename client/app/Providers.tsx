abb3a72b423e600065ddf8b5c5e7f814aba11d31
Deeapk Singh <deepakksingh1202@gmail.com>
2026-05-21 11:50:16 +0530

    chore: update client/app/Providers.tsx

--- client/app/Providers.tsx
+++ client/app/Providers.tsx
@@ -2,11 +2,16 @@
 
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store/reduxStore";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useMemo } from "react";
 export function Providers({ children }: { children: React.ReactNode }) {
+    const queryClient = useMemo(() => new QueryClient(), []);
+
   return (
+      <QueryClientProvider client={queryClient}>
     <ReduxProvider store={store}>
       {children}
     </ReduxProvider>
+    </QueryClientProvider>
   );
 }
